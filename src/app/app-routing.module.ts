a6ae953b09dee5ec29d028fa2e480c900411ad0f
daniel <arutyunyan.daniel23@mail.ru>
2020-08-31 16:12:43 +0300

    Adds components, bootstrap, jq

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: MainLayoutComponent, children: [
+    { path: '', redirectTo: '/', pathMatch: 'full' }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
