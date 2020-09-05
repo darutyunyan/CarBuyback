71c16264967bbef2fed1ede212d709fd4c64e24c
daniel <arutyunyan.daniel23@mail.ru>
2020-09-05 14:23:05 +0300

    Update UI 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { CarouselComponent } from './shared/carousel/carousel.component';
+
+import { AngularYandexMapsModule, IConfig,  } from 'angular8-yandex-maps';
+import { ContactComponent } from './shared/contact/contact.component';
+import { QuestionsComponent } from './shared/questions/questions.component';
+
+
+const mapConfig: IConfig = {
+  apikey: '7fb7e536-2016-4631-b4b1-2111c2675221',
+  lang: 'ru_RU',
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     FooterComponent,
-    HeaderComponent,
     MenuComponent,
-    CarouselComponent
+    CarouselComponent,
+    ContactComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularYandexMapsModule.forRoot(mapConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
